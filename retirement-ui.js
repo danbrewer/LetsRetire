7333a190ecd651c7773d13c6d2122a410b02b332
Dan Brewer <danbrewer@comcast.net>
2026-02-22 10:49:30 -0600

    refactor: update pension button styles and enhance pension section header layout

--- retirement-ui.js
+++ retirement-ui.js
@@ -1771,11 +1771,9 @@ function renderPensionList() {
       <div class="pension-actions">
         <button class="pension-btn pension-edit edit" data-id="${item.id}" title="Modify pension details">
           <span class="btn-icon">📝</span>
-          <span class="btn-text">Edit</span>
         </button>
         <button class="pension-btn pension-delete delete" data-id="${item.id}" title="Remove pension">
           <span class="btn-icon">❌</span>
-          <span class="btn-text">Remove</span>
         </button>
       </div>
     `;
